af0725a6fb86deeb59704764646e86ee20c58b76
Feytox <73577753+feytox@users.noreply.github.com>
2022-07-03 20:44:25 +0500

    increased default max distance

--- src/main/java/name/uwu/feytox/showmeurnick/client/ShowMeUrNickConfig.java
+++ src/main/java/name/uwu/feytox/showmeurnick/client/ShowMeUrNickConfig.java
@@ -6,9 +6,9 @@ public class ShowMeUrNickConfig extends MidnightConfig {
     @Entry
     public static String format = "&6-= &f$NICK$ &6=-";
 
-    @Entry(min=1.0f, max=32.0f)
+    @Entry(min=1.0f, max=64.0f)
     public static float minDistance = 3.0f;
 
-    @Entry(min=1.0f, max=32.0f)
-    public static float maxDistance = 16.0f;
+    @Entry(min=1.0f, max=64.0f)
+    public static float maxDistance = 32.0f;
 }
