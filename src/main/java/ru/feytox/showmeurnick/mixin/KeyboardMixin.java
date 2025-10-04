27cb31ac12ba66612a65aaaa077a61a8a6519f12
Riflusso <vadim2005110@gmail.com>
2025-10-04 22:30:12 +0400

    Update to 1.21.9

--- src/main/java/ru/feytox/showmeurnick/mixin/KeyboardMixin.java
+++ src/main/java/ru/feytox/showmeurnick/mixin/KeyboardMixin.java
@@ -1,5 +1,6 @@
 package ru.feytox.showmeurnick.mixin;
 
+import net.minecraft.client.input.KeyInput;
 import ru.feytox.showmeurnick.client.ShowMeUrNickClient;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
@@ -12,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class KeyboardMixin {
 
     @Inject(method = "onKey", at = @At("RETURN"))
-    public void onOnKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().options.useKey.matchesKey(key, scancode)) {
+    public void onOnKey(long window, int action, KeyInput input, CallbackInfo ci) {
+        if (MinecraftClient.getInstance().options.useKey.matchesKey(input)) {
             ShowMeUrNickClient.showNick();
         }
     }
--- src/main/java/ru/feytox/showmeurnick/mixin/MouseMixin.java
+++ src/main/java/ru/feytox/showmeurnick/mixin/MouseMixin.java
@@ -1,5 +1,10 @@
 package ru.feytox.showmeurnick.mixin;
 
+import net.minecraft.client.gui.Click;
+import net.minecraft.client.input.MouseInput;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
 import ru.feytox.showmeurnick.client.ShowMeUrNickClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
@@ -9,11 +14,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
-public class MouseMixin {
+public abstract class MouseMixin {
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Shadow
+    public abstract double getScaledX(Window window);
+
+    @Shadow
+    public abstract double getScaledY(Window window);
 
     @Inject(method = "onMouseButton", at = @At("RETURN"))
-    public void onOnMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().options.useKey.matchesMouse(button)) {
+    public void onOnMouseButton(long window, MouseInput input, int action, CallbackInfo ci) {
+        Window window2 = client.getWindow();
+        double x = getScaledX(window2);
+        double y = getScaledY(window2);
+        Click click = new Click(x, y, input);
+        if (MinecraftClient.getInstance().options.useKey.matchesMouse(click)) {
             ShowMeUrNickClient.showNick();
         }
     }
